5ea198c4fd888703139ede3ae6693ab7aef75d0f
Epsilon <kumarsampurn@outlook.com>
2023-03-11 14:09:28 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import About from './components/About';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home/>
+      <main className="w-full  relative " >
+        <About className=" absolute min-h-screen" />
+        <Projects className=" absolute min-h-screen" />
+        <Contact className=" absolute min-h-screen" />
+      </main>
+    </>
   );
-}
+};
 
 export default App;
